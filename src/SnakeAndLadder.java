5833194117548fd696f8018d7e592d9e4bba94ca
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-09 17:26:49 +0530

    Added UC2

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,8 +1,21 @@
-
+import java.util.*;
+import java.lang.*;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		int player1 = 0;
-		
+		while(true) {			
+			if(player1 == 100) {
+				break;
+			}
+			else {
+				int max = 6;
+				int min = 1;
+				double roll = (int) Math.floor(Math.random()*(max-min+1)+min);
+				
+				// player1 = 100;
+				
+			}
+		}
 	}
 }
