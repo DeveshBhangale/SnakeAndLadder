bb9d32aaf7fe3d0b39d51ae6dcc85f8acb0f332b
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-09 18:03:04 +0530

    added UC6

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -5,6 +5,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		// Use Case - 1
 		int player1 = 0;
+		int player1cnt = 0;
+		ArrayList<Integer> positions = new ArrayList<Integer>(); 
 		while(true) {			
 			if(player1 == 100) {
 				break;
@@ -15,6 +17,8 @@ public class SnakeAndLadder {
 				int min = 1;
 				int roll = (int) Math.floor(Math.random()*(max-min+1)+min);
 				int optionPlay = (int) Math.floor(Math.random()*(2-0+1)+0);
+				player1cnt += 1; 
+				
 //				System.out.println(roll);
 //				System.out.println(optionPlay);
 //				player1 = 100;
@@ -22,8 +26,9 @@ public class SnakeAndLadder {
 				switch(optionPlay) {
 					
 				case 0: 
-					if((player1 + roll)<100) {
+					if((player1 + roll)<=100) {
 						player1+=roll;
+						
 					}
 				
 				case 1: 
@@ -38,7 +43,12 @@ public class SnakeAndLadder {
 				if(player1 < 0) {
 					player1 = 0;
 				}
+				System.out.println(roll + " " + optionPlay);
+				System.out.println("player1 Position "+player1);
+				positions.add(player1);
 			}
 		}
+		System.out.println("Player1 is winner");
+		System.out.println("Number of rolls required to win were "+ player1cnt);
 	}
 }
